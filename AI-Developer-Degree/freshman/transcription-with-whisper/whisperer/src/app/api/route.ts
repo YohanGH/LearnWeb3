b530da50b8f86bad892baba655fabb65feab8798
YohanGh <YohanGH@proton.me>
2024-09-24 10:37:33 +0200

    Exercice in Learnweb3 Create AI for transcription with whisper

---
+++ AI-Developer-Degree/freshman/transcription-with-whisper/whisperer/src/app/api/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const data = await req.formData();
+  const theFile: File | null = data.get("file") as unknown as File;
+
+  const formData = new FormData();
+  formData.append("file", theFile);
+  formData.append("mdoel", "whisper-1");
+
+  const response = await fetch(
+    "https://api.openai.com/v1/audio/transcriptions",
+    {
+      headers: {
+        Autorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: formData,
+    }
+  );
+  const body = await response.json();
+
+  return NextResponse.json({ output: body })
+}
